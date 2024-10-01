5c330af38aa19408aaf51bbfe1371dd9a9561428
Farok Hossain <farokafs7@gmail.com>
2024-10-02 02:00:23 +0600

    Navbar created

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import Navbar from "./pages/Shared/Navbar";
+
+function App() {
+  return (
+    <>
+      <Navbar />
+      <h1 className="text-green-600 text-5xl text-center ">Happy Flying</h1>
+    </>
+  );
+}
+
+export default App;
